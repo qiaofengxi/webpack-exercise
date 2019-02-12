d03fbb13369286a3447b5f4882a38928a5fcf95e
zhefengjiang <zhefengjiang@easyops.cn>
2019-02-12 18:15:29 +0800

    feat(webpack): 管理输出练习

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from "lodash";
-
+import printMe from "./print";
 
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 };
